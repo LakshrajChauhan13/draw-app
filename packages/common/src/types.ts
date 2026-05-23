88d5529b48ea034395b5ad16190d12c99d7b7c13
LakshrajChauhan13 <lakshrajchauhan13@gmail.com>
2026-05-23 07:08:20 +0530

    feat(packages): feat(packages): refine zod schemas and fix package exports with dist paths
    --zod schema refined in package/common.
    --fixed packages exports and set outDir/rootDir to both /common and
    /backend-common packages.

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -3,17 +3,19 @@ import { flattenError, z } from "zod";
 export const safeSignUpSchema = z.object({
     username: z.string()
         .min(3, "username should be atleast 3 characters")
-        .max(25, "username must be under 25 characters"),
+        .max(25, "username must be under 25 characters")
+        .toLowerCase(),
     email: z
-    .email('Please provide a valid email address'),
+        .email('Please provide a valid email address').toLowerCase(),
     password: z.string()
         .min(8, 'Password should be atleast 8 characters')
         .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-        .regex(/[0-9]/, "Password must contain at least one number")
+        .regex(/[0-9]/, "Password must contain at least one number"),
+    photo: z.string()
 })
 
 export const safeSignInSchema = z.object({
-    email: z.email('Please provide a valid email address'),
+    email: z.email('Please provide a valid email address').toLowerCase(),
     password: z.string()
         .min(8, 'Password should be atleast 8 characters')
         .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
