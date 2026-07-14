39f83118da7a76e0774575604152501db5ae8dc3
LakshrajChauhan13 <lakshrajchauhan13@gmail.com>
2026-07-14 05:49:46 +0530

    feat(common): inferred the zod schema types

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -11,7 +11,7 @@ export const safeSignUpSchema = z.object({
         .min(8, 'Password should be atleast 8 characters')
         .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
         .regex(/[0-9]/, "Password must contain at least one number"),
-    photo: z.string()
+    photo: z.string().optional()
 })
 
 export const safeSignInSchema = z.object({
@@ -25,7 +25,11 @@ export const safeSignInSchema = z.object({
 export const roomSlugSchema = z.object({
     slug: z.string()
         .min(8, "slug should be atleast 8 characters")
-        .max(15, "slug must be under 15 characters")
+        .max(30, "slug must be under 30 characters")
 })
 
 export const flatten_Error = flattenError;
+export { z }
+export type safeSignUpSchemaType = z.infer<typeof safeSignUpSchema>
+export type SafeSignInSchemaType = z.infer<typeof safeSignInSchema>
+export type RoomSlugSchemaType = z.infer<typeof roomSlugSchema>
